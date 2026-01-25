2c705da137e1cc0b6f7ffe04892dcdae49339075
Ankit Kumar <ankitkr14302@gmail.com>
2026-01-26 00:32:30 +0530

    ternary ex 1

--- JS/ternary_ex1.js
+++ JS/ternary_ex1.js
@@ -1,17 +1,17 @@
 const totalMarksScored = 20;
-/*
+
 if(totalMarksScored < 50){
     console.log("You need to work hard.");
 } else {
     console.log("You cleard the exam great!!!!");
 }
-*/
+
 
 //Use ternary operators
 
 // Syntax condition ? yes : no
 
-//(totalMarksScored < 40) ? console.log("You need to work hard") : console.log("You cleared the exam great!!!")
-
+(totalMarksScored < 40) ? console.log("You need to work hard") : console.log("You cleared the exam great!!!")
+// other way
 const result = totalMarksScored < 30 ? "You need to work hard" : "You cleard the exam great!!!"
 console.log(result);
